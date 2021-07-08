c5ca6a0b53d8e4f5d121e305b1a147c10b9d0805
hugonuns <hugo.silva.nunes@hotmail.com>
2021-07-08 15:18:18 +0100

    Safe

--- ExploraIdeias/forms/contact_mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$toEmail = "geral@exploraideias.pt";
-$mailHeaders = "From: " . $_POST["name"] . "<". $_POST["email"] .">\r\n";
-if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
-print "<p class='success'>Enviado com sucesso!</p>";
-} else {
-print "<p class='Error'>Problema ao enviar.</p>";
-}
-?>
